3712730fcd6837338b9a757207190d2b5937bbf9
Arul0107 <arulprakshv0107@gmail.com>
2026-01-05 15:27:03 +0530

    port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use("/api/leads", leadRoutes)
 app.use("/api/admin", adminRoutes)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 50011
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 )
